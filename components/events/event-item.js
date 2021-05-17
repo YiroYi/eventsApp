189816f2a8c4999435861b27002ccccd763b11c0
YiroYi <yiroyi@live.com>
2021-05-16 20:33:31 -0500

    Adding: Button and Icons to Event card

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,8 @@
-import Link from  'next/link';
+import Button from '../ui/button';
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
+
 import classes from './event-item.module.css';
 
 const EventItems = props => {
@@ -20,14 +24,21 @@ const EventItems = props => {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon/>
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon/>
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <span>
+            <Button link={exploreLink}>Explore Event</Button>
+          </span>
+          <span className={classes.icon}>
+            <ArrowRightIcon/>
+          </span>
         </div>
       </div>
     </li>
